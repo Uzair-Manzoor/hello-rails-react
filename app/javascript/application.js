f7fbf8bbda5c805a638a916bc11ab9941ed2bca7
Uzair-Manzoor <uzair.kayani555@gmail.com>
2024-01-31 17:27:05 +0500

    create static view file and greeting component

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,11 @@
-// app/javascript/application.js
-console.log('Hello world');
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./App";
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root'),
+);
